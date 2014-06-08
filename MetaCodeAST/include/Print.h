e66c8a2492e9b893a51c8e57201e54f46caa5845
Ismael-belghiti <isma.belghiti@gmailcom>
2014-06-08 14:48:52 +0200

    add basic print

--- MetaCodeAST/include/Print.h
+++ MetaCodeAST/include/Print.h
@@ -3,32 +3,47 @@
 
 #include "Expression.h"
 
-/*
 class Printable : public CodeNode {
 public:
    virtual bool isExpression() = 0;
 
-   virtual std::string GetString() { return std::string(); b}
+   virtual std::string GetString() { return std::string(); }
 
-   virtual TypedExpression* GetTypedExpression() { return 0 };
-};
-
-class PrintableFromExpression : public Printable {
-public:
-   PrintableFromExpression(TypedExpression * typedExpr) { 
-      m_typedExpression = typedExpr;
-   }
-
-   virtual bool isExpression() { return true; }
-
-   virtual Expression* GetExpression() { return m_expr; }
+   virtual Expression* GetExpression() { return 0; }
 };
 
 class PrintableFromString : public Printable {
 public:
+   PrintableFromString(std::string text) { m_text = text; }
+
    virtual bool isExpression() { return false; }
+
+   virtual std::string GetString() { return m_text; }
+
+   virtual void PrintDebug(int level = 0) {
+      printIndent(level);
+      std::cout << GetString();
+   }
+
+private:
+   std::string m_text;
 };
-*/
+
+class PrintableFromExpression : public Printable {
+public:
+   PrintableFromExpression(Expression *expr) { m_expr = expr; }
+
+   virtual bool isExpression() { return true; }
+
+   virtual Expression* GetExpression() { return m_expr; }
+
+   virtual void PrintDebug(int level = 0) {
+      GetExpression()->PrintDebug(level);
+   }
+
+private:
+   Expression* m_expr;
+};
 
 class Print : public CodeNode {
 public:
