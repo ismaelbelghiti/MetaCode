c8e62d2ff627180a7a8cc48e92edb27d2f46e19e
Ismael-belghiti <isma.belghiti@gmailcom>
2014-06-09 01:55:05 +0200

    work in progress for Function

--- MetaCodeAST/include/Function.h
+++ MetaCodeAST/include/Function.h
@@ -4,19 +4,37 @@
 #include "Variable.h"
 #include "Bloc.h"
 
+class Function : public CodeNode {
+public:
+   Function(Type* type, std::string name) {
+      m_type = type;
+      m_name = name; 
+   }
+
+  virtual Type* GetType() { return m_type; }
+
+  virtual std::string GetName() { return m_name; }
+
+  virtual void PrintDebug(int level = 0) {
+     printIndent(level);
+     std::cout << GetName();
+  }
+
+private:
+  Type* m_type;
+  std::string m_name;
+};
+
 class FunctionDeclaration : public CodeNode {
 public:
-   Function(Type* type, std::string name, Variable** arg, int nbArgs,  Bloc* bloc) {
-      m_type = type;
-      m_name = name;
+   FunctionDeclaration(Function* func, Variable** arg, int nbArgs,  Bloc* bloc) {
+      m_func = func;
       m_arg = arg;
       m_nbArgs = nbArgs;
       m_bloc = bloc;
    }
 
-   virtual Type* GetType() { return m_type; }
-
-   virtual std::string GetName() { return m_name; }
+   virtual Function* GetFunction() { return m_func; }
 
    virtual int GetNbArgs() { return m_nbArgs; }
 
@@ -24,14 +42,28 @@ public:
 
    virtual Bloc* GetBloc() { return m_bloc; }
 
+   virtual void PrintDebug(int level = 0) {
+      printIndent(level);
+      std::cout << "Function ";
+      GetFunction()->PrintDebug();
+      std::cout << " (";
+      for(int iArg = 0; iArg < GetNbArgs(); iArg++) {
+	 GetArg(iArg)->GetType()->PrintDebug();
+	 std::cout << " ";
+	 GetArg(iArg)->PrintDebug();
+	 if(iArg < GetNbArgs()-1)
+	    std::cout << ",";
+      }
+      std::cout << ")";
+   }
+
 private:
-   Type* m_type;
-   std::string m_name;
+   Function *m_func;
    Variable** m_arg;
    int m_nbArgs;
    Bloc* m_bloc;
 };
 
-
+//class FunctionCall
 
 #endif
