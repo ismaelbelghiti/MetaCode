7cd9f6f32e4c6014edfb1fff0087b5683c7c07e2
Ismael-belghiti <isma.belghiti@gmail.com>
2014-06-15 17:53:17 +0200

    add Expression.cpp

--- MetaCodeAST/include/MetaCodeAST/Expression.h
+++ MetaCodeAST/include/MetaCodeAST/Expression.h
@@ -10,16 +10,13 @@ public:
 
 class ExprFromInt : public Expression {
 public:
-   ExprFromInt(int value) { m_value = value; }
+   ExprFromInt(int value);
 
-   virtual int GetValue() { return m_value; }
+   virtual int GetValue();
 
-   virtual Type* GetType() { return cInt; }
+   virtual Type* GetType();
 
-   virtual void PrintDebug(int level = 0) {
-      printIndent(level);
-      std::cout << GetValue();
-   }
+   virtual void PrintDebug(int level = 0);
 
 private:
    int m_value;
@@ -27,35 +24,27 @@ private:
 
 class ExprFromBool : public Expression {
 public:
-   ExprFromBool(int value) { m_value = value; }
+   ExprFromBool(int value);
 
-   virtual bool GetValue() { return m_value; }
+   virtual bool GetValue();
 
-   virtual Type* GetType() { return cBool; }
+   virtual Type* GetType();
 
-   virtual void PrintDebug(int level = 0) {
-      printIndent(level);
-      if(m_value)
-	 std::cout << "True";
-      else
-	 std::cout << "False";
-   }
+   virtual void PrintDebug(int level = 0);
+
 private:
    bool m_value;
 };
 
 class ExprFromVariable : public Expression {
 public:
-   ExprFromVariable(Variable* variable) { m_variable = variable; }
+   ExprFromVariable(Variable* variable);
 
-   Variable* GetVariable() { return m_variable; }
+   Variable* GetVariable();
 
-   virtual Type* GetType() { return GetVariable()->GetType(); }
+   virtual Type* GetType();
 
-   virtual void PrintDebug(int level = 0) {
-      printIndent(level);
-      GetVariable()->PrintDebug();
-   }
+   virtual void PrintDebug(int level = 0);
 
 private:
    Variable* m_variable;
@@ -67,9 +56,9 @@ private:
 
 class UnaryOperation : public Expression {
 public:
-   UnaryOperation(Expression* expr) { m_expr = expr; }
+   UnaryOperation(Expression* expr);
 
-   virtual Expression* GetExpression() { return m_expr; }
+   virtual Expression* GetExpression();
 
 private:
    Expression* m_expr;
@@ -77,16 +66,11 @@ private:
 
 class ParenthesizedExpr : public UnaryOperation {
 public:
-   ParenthesizedExpr(Expression* expr) : UnaryOperation(expr) { }
+   ParenthesizedExpr(Expression* expr);
 
-   virtual Type* GetType() { return GetExpression()->GetType(); }
+   virtual Type* GetType();
 
-   virtual void PrintDebug(int level = 0) {
-      printIndent(level);
-      std::cout << "(";
-      GetExpression()->PrintDebug();
-      std::cout << ")";
-   }
+   virtual void PrintDebug(int level = 0);
 
 private:
    Expression* m_expr;
@@ -94,28 +78,20 @@ private:
 
 class Minus : public UnaryOperation {
 public:
-   Minus(Expression* expr) : UnaryOperation(expr) {}
+   Minus(Expression* expr);
 
-   virtual Type* GetType() { return GetExpression()->GetType(); }
+   virtual Type* GetType();
 
-   virtual void PrintDebug(int level = 0) {
-      printIndent(level);
-      std::cout << "-";
-      GetExpression()->PrintDebug();
-   }
+   virtual void PrintDebug(int level = 0);
 };
 
 class Negation : public UnaryOperation {
 public:
-   Negation(Expression* expr) : UnaryOperation(expr) {}
+   Negation(Expression* expr);
 
-   virtual Type* GetType() { return cBool; }
+   virtual Type* GetType();
 
-   virtual void PrintDebug(int level = 0) {
-      printIndent(level);
-      std::cout << "!";
-      GetExpression()->PrintDebug();
-   }
+   virtual void PrintDebug(int level = 0);
 };
 
 //////////////////////
@@ -124,23 +100,15 @@ public:
 
 class BinaryOperation : public Expression {
 public:
-   BinaryOperation(Expression* leftExpr, Expression* rightExpr) {
-      m_leftExpr = leftExpr;
-      m_rightExpr = rightExpr;
-   }
+   BinaryOperation(Expression* leftExpr, Expression* rightExpr);
 
-   Expression* GetLeftExpr() { return m_leftExpr; }
+   Expression* GetLeftExpr();
 
-   Expression* GetRightExpr() { return m_rightExpr; }
+   Expression* GetRightExpr();
    
    virtual std::string GetSymbol() = 0;
 
-   virtual void PrintDebug(int level = 0) {
-      printIndent(level);
-      GetLeftExpr()->PrintDebug();
-      std::cout << " " << GetSymbol() << " ";
-      GetRightExpr()->PrintDebug();
-   }
+   virtual void PrintDebug(int level = 0);
 
 private:
    Expression *m_leftExpr, *m_rightExpr;
@@ -148,73 +116,66 @@ private:
 
 class Addition : public BinaryOperation {
 public:
-   Addition(Expression* leftExpr, Expression* rightExpr) 
-      : BinaryOperation(leftExpr,rightExpr) {}
+   Addition(Expression* leftExpr, Expression* rightExpr);
    
-   virtual Type* GetType() { return GetLeftExpr()->GetType(); }
+   virtual Type* GetType();
 
-   virtual std::string GetSymbol() { return "+"; }
+   virtual std::string GetSymbol();
 };
 
 class Multiplication : public BinaryOperation {
 public:
-   Multiplication(Expression* leftExpr, Expression* rightExpr) 
-      : BinaryOperation(leftExpr,rightExpr) {}
+   Multiplication(Expression* leftExpr, Expression* rightExpr);
 
-   virtual Type* GetType() { return GetLeftExpr()->GetType(); }
+   virtual Type* GetType();
 
-   virtual std::string GetSymbol() { return "*"; }
+   virtual std::string GetSymbol();
 };
 
 class Substraction : public BinaryOperation {
 public:
-   Substraction(Expression* leftExpr, Expression* rightExpr) 
-      : BinaryOperation(leftExpr,rightExpr) {}
+   Substraction(Expression* leftExpr, Expression* rightExpr);
 
-   virtual Type* GetType() { return GetLeftExpr()->GetType(); }
+   virtual Type* GetType();
 
-   virtual std::string GetSymbol() { return "-"; }
+   virtual std::string GetSymbol();
 };
 
 class EuclidianDivision : public BinaryOperation {
 public:
-   EuclidianDivision(Expression* leftExpr, Expression* rightExpr) 
-      : BinaryOperation(leftExpr,rightExpr) {}
+   EuclidianDivision(Expression* leftExpr, Expression* rightExpr);
 
-   virtual Type* GetType() { return cInt; }
+   virtual Type* GetType();
 
-   virtual std::string GetSymbol() { return "/"; }
+   virtual std::string GetSymbol();
 };
 
 
 class Modulus : public BinaryOperation {
 public:
-   Modulus(Expression* leftExpr, Expression* rightExpr) 
-      : BinaryOperation(leftExpr,rightExpr) {}
+   Modulus(Expression* leftExpr, Expression* rightExpr);
 
-   virtual Type* GetType() { return cInt; }
+   virtual Type* GetType();
 
-   virtual std::string GetSymbol() { return "%"; }
+   virtual std::string GetSymbol();
 };
 
 class And : public BinaryOperation {
 public:
-   And(Expression* leftExpr, Expression* rightExpr) 
-      : BinaryOperation(leftExpr,rightExpr) {}
+   And(Expression* leftExpr, Expression* rightExpr);
 
-   virtual Type* GetType() { return cBool; }
+   virtual Type* GetType();
 
-   virtual std::string GetSymbol() { return "&&"; }
+   virtual std::string GetSymbol();
 };
 
 class Or : public BinaryOperation {
 public:
-   Or(Expression* leftExpr, Expression* rightExpr) 
-      : BinaryOperation(leftExpr,rightExpr) {}
+   Or(Expression* leftExpr, Expression* rightExpr);
 
-   virtual Type* GetType() { return cBool; }
+   virtual Type* GetType();
 
-   virtual std::string GetSymbol() { return "||"; }
+   virtual std::string GetSymbol();
 };
 
 #endif
---
+++ MetaCodeAST/src/Expression.cpp
@@ -0,0 +1,214 @@
+#include "MetaCodeAST/Expression.h"
+
+ExprFromInt::ExprFromInt(int value) {
+   m_value = value;
+}
+
+int ExprFromInt::GetValue() {
+   return m_value;
+}
+
+Type* ExprFromInt::GetType() {
+   return cInt;
+}
+
+void ExprFromInt::PrintDebug(int level) {
+   printIndent(level);
+   std::cout << GetValue();
+}
+
+
+ExprFromBool::ExprFromBool(int value) {
+   m_value = value;
+}
+
+bool ExprFromBool::GetValue() {
+   return m_value;
+}
+
+Type* ExprFromBool::GetType() {
+   return cBool;
+}
+
+void ExprFromBool::PrintDebug(int level) {
+   printIndent(level);
+   if(m_value)
+      std::cout << "True";
+   else
+      std::cout << "False";
+}
+
+
+
+ExprFromVariable::ExprFromVariable(Variable* variable) {
+   m_variable = variable;
+}
+
+Variable* ExprFromVariable::GetVariable() {
+   return m_variable;
+}
+
+Type* ExprFromVariable::GetType() {
+   return GetVariable()->GetType();
+}
+
+void ExprFromVariable::PrintDebug(int level) {
+   printIndent(level);
+   GetVariable()->PrintDebug();
+}
+
+
+/////////////////////
+// Unary Operation //
+/////////////////////
+
+
+UnaryOperation::UnaryOperation(Expression* expr) {
+   m_expr = expr;
+}
+
+Expression* UnaryOperation::GetExpression() {
+   return m_expr;
+}
+
+
+ParenthesizedExpr::ParenthesizedExpr(Expression* expr) : UnaryOperation(expr) { }
+
+Type* ParenthesizedExpr::GetType() {
+   return GetExpression()->GetType();
+}
+
+void ParenthesizedExpr::PrintDebug(int level) {
+   printIndent(level);
+   std::cout << "(";
+   GetExpression()->PrintDebug();
+   std::cout << ")";
+}
+
+
+Minus::Minus(Expression* expr) : UnaryOperation(expr) {}
+
+Type* Minus::GetType() {
+   return GetExpression()->GetType();
+}
+
+void Minus::PrintDebug(int level) {
+   printIndent(level);
+   std::cout << "-";
+   GetExpression()->PrintDebug();
+}
+
+   
+Negation::Negation(Expression* expr) : UnaryOperation(expr) {}
+
+Type* Negation::GetType() {
+   return cBool;
+}
+
+void Negation::PrintDebug(int level) {
+   printIndent(level);
+   std::cout << "!";
+   GetExpression()->PrintDebug();
+}
+
+
+//////////////////////
+// Binary Operation //
+//////////////////////
+
+BinaryOperation::BinaryOperation(Expression* leftExpr, Expression* rightExpr) {
+   m_leftExpr = leftExpr;
+   m_rightExpr = rightExpr;
+}
+
+Expression* BinaryOperation::GetLeftExpr() {
+   return m_leftExpr;
+}
+
+Expression* BinaryOperation::GetRightExpr() {
+   return m_rightExpr;
+}
+
+void BinaryOperation::PrintDebug(int level) {
+   printIndent(level);
+   GetLeftExpr()->PrintDebug();
+   std::cout << " " << GetSymbol() << " ";
+   GetRightExpr()->PrintDebug();
+}
+
+
+Addition::Addition(Expression* leftExpr, Expression* rightExpr) 
+   : BinaryOperation(leftExpr,rightExpr) {}
+   
+Type* Addition::GetType() {
+   return GetLeftExpr()->GetType();
+}
+
+std::string Addition::GetSymbol() {
+   return "+";
+}
+
+
+Multiplication::Multiplication(Expression* leftExpr, Expression* rightExpr) 
+   : BinaryOperation(leftExpr,rightExpr) {}
+
+Type* Multiplication::GetType() {
+   return GetLeftExpr()->GetType();
+}
+
+std::string Multiplication::GetSymbol() {
+   return "*";
+}
+
+
+Substraction::Substraction(Expression* leftExpr, Expression* rightExpr) 
+   : BinaryOperation(leftExpr,rightExpr) {}
+
+Type* Substraction::GetType() {
+   return GetLeftExpr()->GetType();
+}
+
+std::string Substraction::GetSymbol() {
+   return "-";
+}
+
+
+EuclidianDivision::EuclidianDivision(Expression* leftExpr, Expression* rightExpr) 
+   : BinaryOperation(leftExpr,rightExpr) {}
+
+Type* EuclidianDivision::GetType() {
+   return cInt;
+}
+
+std::string EuclidianDivision::GetSymbol() {
+   return "/";
+}
+
+
+Modulus::Modulus(Expression* leftExpr, Expression* rightExpr) 
+   : BinaryOperation(leftExpr,rightExpr) {}
+
+Type* Modulus::GetType() {
+   return cInt;
+}
+
+std::string Modulus::GetSymbol() {
+   return "%";
+}
+
+
+And::And(Expression* leftExpr, Expression* rightExpr) 
+   : BinaryOperation(leftExpr,rightExpr) {}
+
+Type* And::GetType() { return cBool; }
+
+std::string And::GetSymbol() { return "&&"; }
+
+
+Or::Or(Expression* leftExpr, Expression* rightExpr) 
+   : BinaryOperation(leftExpr,rightExpr) {}
+
+Type* Or::GetType() { return cBool; }
+
+std::string Or::GetSymbol() { return "||"; }
+
