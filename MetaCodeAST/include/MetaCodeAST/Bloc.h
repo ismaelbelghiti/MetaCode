e53f7528f0aae6ca3c42da3aef4084ca054f2100
Ismael-belghiti <isma.belghiti@gmail.com>
2014-06-15 16:54:07 +0200

    add Bloc.cpp

--- MetaCodeAST/include/MetaCodeAST/Bloc.h
+++ MetaCodeAST/include/MetaCodeAST/Bloc.h
@@ -5,24 +5,13 @@
 
 class Bloc : public CodeNode {
 public:
-   Bloc(CodeNode** codeNode, int nbCodeNodes) {
-      m_codeNode = codeNode; 
-      m_nbCodeNodes = nbCodeNodes;
-   }
+   Bloc(CodeNode** codeNode, int nbCodeNodes);
 
-   virtual int GetNbCodeNodes() { return m_nbCodeNodes; }
+   virtual int GetNbCodeNodes();
 
-   virtual CodeNode* GetCodeNode(int iCodeNode) { return m_codeNode[iCodeNode];  }
+   virtual CodeNode* GetCodeNode(int iCodeNode);
 
-   virtual void PrintDebug(int level = 0) {
-      printIndent(level);
-      std::cout << "{" << std::endl;
-      for(int iCodeNode = 0; iCodeNode < GetNbCodeNodes(); iCodeNode++) {
-	 GetCodeNode(iCodeNode)->PrintDebug(level+1);
-	 std::cout << std::endl;
-      }
-      std::cout << "}" << std::endl;
-   }
+   virtual void PrintDebug(int level = 0);
 
 private:
    CodeNode** m_codeNode;
---
+++ MetaCodeAST/src/Bloc.cpp
@@ -0,0 +1,24 @@
+#include "MetaCodeAST/Bloc.h"
+
+Bloc::Bloc(CodeNode** codeNode, int nbCodeNodes) {
+   m_codeNode = codeNode; 
+   m_nbCodeNodes = nbCodeNodes;
+}
+
+int Bloc::GetNbCodeNodes() {
+   return m_nbCodeNodes; 
+}
+
+CodeNode* Bloc::GetCodeNode(int iCodeNode) {
+   return m_codeNode[iCodeNode];  
+}
+
+void Bloc::PrintDebug(int level) {
+   printIndent(level);
+   std::cout << "{" << std::endl;
+   for(int iCodeNode = 0; iCodeNode < GetNbCodeNodes(); iCodeNode++) {
+      GetCodeNode(iCodeNode)->PrintDebug(level+1);
+      std::cout << std::endl;
+   }
+   std::cout << "}" << std::endl;
+}
