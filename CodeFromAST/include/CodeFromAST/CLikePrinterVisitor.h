96e882f1d021dd06063a6a27525ba7adb9de34e6
Ismael-belghiti <isma.belghiti@gmail.com>
2014-06-17 19:44:36 +0200

    add missing files

---
+++ CodeFromAST/include/CodeFromAST/CLikePrinterVisitor.h
@@ -0,0 +1,81 @@
+#ifndef CLIKE_PRINTER_VISITOR_H
+#define CLIKE_PRINTER_VISITOR_H
+
+#include "MetaCodeAST/AbstractVisitor.h"
+
+class CLikePrinterVisitor : public AbstractVisitor {
+public:
+   enum Language { C, CPP, JAVA, JVS };
+
+   CLikePrinterVisitor(Language lang);
+
+   virtual void VisitExprFromInt(ExprFromInt* exprFromInt);
+
+   virtual void VisitExprFromBool(ExprFromBool* exprFromBool);
+
+   virtual void VisitExprFromVariable(ExprFromVariable* exprFromVariable);
+
+   virtual void VisitParenthesizedExpr(ParenthesizedExpr* parenthesizedExpr);
+
+   virtual void VisitMinus(Minus* minus);
+
+   virtual void VisitNegation(Negation* negation);
+
+   virtual void VisitAddition(Addition * addition);
+
+   virtual void VisitMultiplication(Multiplication * multiplication);
+
+   virtual void VisitSubstraction(Substraction * substraction);
+
+   virtual void VisitEuclidianDivision(EuclidianDivision * euclDiv);
+
+   virtual void VisitModulus(Modulus * modulus);
+
+   virtual void VisitAnd(And * andNode);
+
+   virtual void VisitOr(Or * orNode);
+
+   virtual void VisitBloc(Bloc* bloc);
+
+   virtual void VisitIf(If * ifNode);
+
+   virtual void VisitElseIf(ElseIf * elseIfNode);
+
+   virtual void VisitElse(Else * elseNode);
+
+   virtual void VisitDeclaration(Declaration * decl);
+
+   virtual void VisitRange(Range * range);
+
+   virtual void VisitFor(For * forNode);
+
+   virtual void VisitFunction(Function * function);
+
+   virtual void VisitSignature(Signature * signature);
+
+   virtual void VisitFunctionDeclaration(FunctionDeclaration * functionDecl);
+
+   virtual void VisitIncludeLib(IncludeLib* includeLib);
+
+   virtual void VisitPrintableFromString(PrintableFromString * printStr);
+
+   virtual void VisitPrintableFromExpression(PrintableFromExpression * printExpr);
+
+   virtual void VisitPrint(Print * print);
+
+   virtual void VisitType(Type * type);
+
+   virtual void VisitVariable(Variable* variable);
+
+   virtual void VisitWhile(While* whileNode);
+   
+   virtual void VisitMain(Main* mainNode);
+
+private:
+   virtual void VisitBinaryOperation(BinaryOperation* binOp, std::string opString);
+
+   Language m_lang;
+};
+
+#endif
+
---
+++ CodeFromAST/src/CLikePrinterVisitor.cpp
@@ -0,0 +1,185 @@
+#include "CodeFromAST/CLikePrinterVisitor.h"
+
+CLikePrinterVisitor::CLikePrinterVisitor(CLikePrinterVisitor::Language lang) {
+   m_lang = lang;
+}
+
+void CLikePrinterVisitor::VisitExprFromInt(ExprFromInt* exprFromInt) {
+   std::cout << exprFromInt->GetValue();
+}
+
+void CLikePrinterVisitor::VisitExprFromBool(ExprFromBool* exprFromBool) {
+   if(exprFromBool->GetValue())
+      std::cout << "Vrai";
+   else
+      std::cout << "Faux";
+}
+
+void CLikePrinterVisitor::VisitExprFromVariable(ExprFromVariable* exprFromVariable) {
+   std::cout << exprFromVariable->GetVariable()->GetName();
+}
+
+void CLikePrinterVisitor::VisitParenthesizedExpr(ParenthesizedExpr* parenthesizedExpr) {
+   std::cout << "(";
+   parenthesizedExpr->GetExpression()->Visit(this);
+   std::cout << ")";
+}
+
+void CLikePrinterVisitor::VisitMinus(Minus* minus) {
+   std::cout << "-";
+   minus->GetExpression()->Visit(this);
+}
+
+void CLikePrinterVisitor::VisitNegation(Negation* negation) {
+   std::cout << "!";
+   negation->GetExpression()->Visit(this);
+}
+
+void CLikePrinterVisitor::VisitAddition(Addition * addition) {
+   VisitBinaryOperation(addition, " + ");
+}
+
+void CLikePrinterVisitor::VisitMultiplication(Multiplication * multiplication) {
+   VisitBinaryOperation(multiplication, " * ");
+}
+
+void CLikePrinterVisitor::VisitSubstraction(Substraction * substraction) {
+   VisitBinaryOperation(substraction, " - ");
+}
+
+void CLikePrinterVisitor::VisitEuclidianDivision(EuclidianDivision * euclDiv) {
+   VisitBinaryOperation(euclDiv, " / ");
+}
+
+void CLikePrinterVisitor::VisitModulus(Modulus * modulus) {
+   VisitBinaryOperation(modulus, " modulo ");
+}
+
+void CLikePrinterVisitor::VisitAnd(And * andNode) {
+   VisitBinaryOperation(andNode, " et ");
+}
+
+void CLikePrinterVisitor::VisitOr(Or * orNode) {
+   VisitBinaryOperation(orNode, " ou ");
+}
+
+void CLikePrinterVisitor::VisitBloc(Bloc* bloc) {
+   for(int iCodeNode = 0; iCodeNode < bloc->GetNbCodeNodes(); iCodeNode++) {
+     bloc->GetCodeNode(iCodeNode)->Visit(this);
+      std::cout << std::endl;
+   }
+}
+
+void CLikePrinterVisitor::VisitIf(If * ifNode) {
+   std::cout << "Si ";
+   ifNode->GetCondition()->Visit(this);
+   std::cout << std::endl;
+}
+
+void CLikePrinterVisitor::VisitElseIf(ElseIf * elseIfNode)  {
+   std::cout << "Sinon si ";
+   elseIfNode->GetCondition()->Visit(this);
+   std::cout << std::endl;
+}
+
+void CLikePrinterVisitor::VisitElse(Else * elseNode) {
+   std::cout << "Sinon " << std::endl;
+   elseNode->GetBloc()->Visit(this);
+}
+
+void CLikePrinterVisitor::VisitDeclaration(Declaration * decl) {
+   decl->GetVariable()->GetType()->Visit(this);
+   std::cout << " ";
+   decl->GetVariable()->Visit(this);
+   std::cout << " = ";
+   decl->GetExpression()->Visit(this);
+   std::cout << std::endl;
+}
+
+void CLikePrinterVisitor::VisitRange(Range * range) {
+   std::cout << " partant de ";
+   range->GetStart()->Visit(this);
+   if(range->IsIncreasing())
+      std::cout << " et croissant jusque ";
+   else
+      std::cout << " down to ";
+   if(range->IncludedEndIsDefined()){
+      range->GetIncludedEnd()->Visit(this);
+      std::cout << " inclu ";
+   }
+   else {
+      range->GetExcludedEnd()->Visit(this);
+      std::cout << " exclu ";
+   }
+}
+
+void CLikePrinterVisitor::VisitFor(For * forNode) {
+   std::cout << "Pour ";
+   forNode->GetVariable()->Visit(this);
+   forNode->GetRange()->Visit(this);
+   std::cout << std::endl;
+   forNode->GetBloc()->Visit(this);
+}
+
+void CLikePrinterVisitor::VisitFunction(Function * function) {
+   //todo
+}
+
+void CLikePrinterVisitor::VisitSignature(Signature * signature) {
+   //todo
+}
+
+void CLikePrinterVisitor::VisitFunctionDeclaration(FunctionDeclaration * functionDecl) {
+   //todo
+}
+
+void CLikePrinterVisitor::VisitIncludeLib(IncludeLib* includeLib) {
+   //todo
+}
+
+void CLikePrinterVisitor::VisitPrintableFromString(PrintableFromString * printStr) {
+   std::cout << "\""  << printStr->GetString() << "\"";
+}
+
+void CLikePrinterVisitor::VisitPrintableFromExpression(PrintableFromExpression * printExpr) {
+   printExpr->GetExpression()->Visit(this);
+}
+
+void CLikePrinterVisitor::VisitPrint(Print * print) {
+   std::cout << "Affiche ";
+   for(int iPrintable = 0; iPrintable < print->GetNbPrintables(); iPrintable++) {
+      print->GetPrintable(iPrintable)->Visit(this);
+      std::cout << " ";
+   }	 
+   if(print->WithEndline())
+      std::cout << " et passer a la ligne";
+   std::cout << std::endl;
+}
+
+void CLikePrinterVisitor::VisitType(Type * type) {
+   std::cout << type->GetName();
+}
+
+void CLikePrinterVisitor::VisitVariable(Variable* variable)  {
+   std::cout << variable->GetName();
+}
+
+void CLikePrinterVisitor::VisitWhile(While* whileNode) {
+   std::cout << "Tant que ";
+   whileNode->GetCondition()->Visit(this);
+   std::cout << std::endl;
+   whileNode->GetBloc()->Visit(this);
+}
+   
+void CLikePrinterVisitor::VisitMain(Main* mainNode) {
+   //todo
+}
+
+
+//private:
+void CLikePrinterVisitor::VisitBinaryOperation(BinaryOperation* binOp, std::string opString) {
+   binOp->GetLeftExpr()->Visit(this);
+   std::cout << opString;
+   binOp->GetRightExpr()->Visit(this);
+}
+
