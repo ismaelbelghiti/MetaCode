0de061ee717a5f7bc803e51c6ab76a602eada519
Ismael-belghiti <isma.belghiti@gmail.com>
2014-06-21 19:11:31 +0200

    wip CodeExpander

--- CodeFromAST/include/CodeFromAST/CodeExpander.h
+++ CodeFromAST/include/CodeFromAST/CodeExpander.h
@@ -2,7 +2,7 @@
 #define CODE_EXPANDER_H
 
 #include <vector>
-#include "MetaCodeAST/AbstractVisitor.h"
+#include "MetaCodeAST.h"
 
 /**
  * Class used for the really first preprocess of the AST. It aims
@@ -11,78 +11,9 @@
  */
 class CodeExpander : public AbstractVisitor {
 public:
-   virtual CodeNode* Expand(CodeNode* root);
 
-   virtual void PushResult(CodeNode* codeNode);
+   virtual Expression* ExpandExpression(Expression* expr);
 
-   virtual CodeNode* PopResult();
-
-// Visits
-
-   virtual void VisitExprFromInt(ExprFromInt* exprFromInt);
-
-   virtual void VisitExprFromBool(ExprFromBool* exprFromBool);
-
-   virtual void VisitExprFromVariable(ExprFromVariable* exprFromVariable);
-
-   virtual void VisitParenthesizedExpr(ParenthesizedExpr* parenthesizedExpr);
-
-   virtual void VisitMinus(Minus* minus);
-
-   virtual void VisitNegation(Negation* negation);
-
-   virtual void VisitAddition(Addition * addition);
-
-   virtual void VisitMultiplication(Multiplication * multiplication);
-
-   virtual void VisitSubstraction(Substraction * substraction);
-
-   virtual void VisitEuclidianDivision(EuclidianDivision * euclDiv);
-
-   virtual void VisitModulus(Modulus * modulus);
-
-   virtual void VisitAnd(And * andNode);
-
-   virtual void VisitOr(Or * orNode);
-
-   virtual void VisitBloc(Bloc* bloc);
-
-   virtual void VisitIf(If * ifNode);
-
-   virtual void VisitElseIf(ElseIf * elseIfNode);
-
-   virtual void VisitElse(Else * elseNode);
-
-   virtual void VisitDeclaration(Declaration * decl);
-
-   virtual void VisitRange(Range * range);
-
-   virtual void VisitFor(For * forNode);
-
-   virtual void VisitFunction(Function * function);
-
-   virtual void VisitSignature(Signature * signature);
-
-   virtual void VisitFunctionDeclaration(FunctionDeclaration * functionDecl);
-
-   virtual void VisitIncludeLib(IncludeLib* includeLib);
-
-   virtual void VisitPrintableFromString(PrintableFromString * printStr);
-
-   virtual void VisitPrintableFromExpression(PrintableFromExpression * printExpr);
-
-   virtual void VisitPrint(Print * print);
-
-   virtual void VisitType(Type * type);
-
-   virtual void VisitVariable(Variable* variable);
-
-   virtual void VisitWhile(While* whileNode);
-   
-   virtual void VisitMain(Main* mainNode);
-
-private:
-   std::vector<CodeNode*> m_result;
 };
 
 #endif
--- CodeFromAST/src/CodeExpander.cpp
+++ CodeFromAST/src/CodeExpander.cpp
@@ -1,91 +1,37 @@
 #include "CodeFromAST/CodeExpander.h"
+#include "CodeFromAST/EmptyVisitor.h"
 
+class ExpressionExpander : public EmptyVisitor {
+public:
 
-CodeNode* CodeExpander::Expand(CodeNode* root) {
-   root->Visit(this);
-   return PopResult();
-}
+   ExpressionExpander(CodeExpander* codeExpander) {
+      m_codeExpander = codeExpander;
+   }
 
-void CodeExpander::PushResult(CodeNode* codeNode) {
-   m_result.push_back(codeNode);
-}
+   virtual void VisitAddition(Addition* add) {
+      Expression* left = 
+	 m_codeExpander->ExpandExpression(add->GetLeftExpr());
+      Expression* right =
+	 m_codeExpander->ExpandExpression(add->GetRightExpr());
+      m_result = new Addition(left,right);
+   }
 
-CodeNode* CodeExpander::PopResult() {
-   CodeNode* result = m_result.back();
-   m_result.pop_back();
+   // TODO treat them all
+
+   Expression* GetResult() {
+      return m_result;
+   };
+
+private:
+   CodeExpander* m_codeExpander;
+   Expression* m_result;
+};
+
+Expression* CodeExpander::ExpandExpression(Expression* expr) {
+   ExpressionExpander* exprExpander = new ExpressionExpander(this);
+   expr->Visit(exprExpander);
+   Expression* result = exprExpander->GetResult();
+   delete exprExpander;
    return result;
 }
 
-
-void CodeExpander::VisitExprFromInt(ExprFromInt* exprFromInt) {
-}
-
-void CodeExpander::VisitExprFromBool(ExprFromBool* exprFromBool) {
-}
-
-void CodeExpander::VisitExprFromVariable(ExprFromVariable* exprFromVariable) {
-}
-
-void CodeExpander::VisitParenthesizedExpr(ParenthesizedExpr* parenthesizedExpr) {
-}
-
-void CodeExpander::VisitMinus(Minus* minus) {}
-
-void CodeExpander::VisitNegation(Negation* negation) {}
-
-void CodeExpander::VisitAddition(Addition * addition) {}
-
-void CodeExpander::VisitMultiplication(Multiplication * multiplication) {}
-
-void CodeExpander::VisitSubstraction(Substraction * substraction) {}
-
-void CodeExpander::VisitEuclidianDivision(EuclidianDivision * euclDiv) {}
-
-void CodeExpander::VisitModulus(Modulus * modulus) {}
-
-void CodeExpander::VisitAnd(And * andNode) {}
-
-void CodeExpander::VisitOr(Or * orNode) {}
-
-void CodeExpander::VisitBloc(Bloc* bloc) {
-   int nbCodeNodes = bloc->GetNbCodeNodes();
-   CodeNode** codeNode = new CodeNode*[nbCodeNodes];
-   for(int iCodeNode = 0; iCodeNode < nbCodeNodes; iCodeNode++) {
-      codeNode[iCodeNode] = Expand(bloc->GetCodeNode(iCodeNode));
-   }
-   PushResult(new Bloc(codeNode,nbCodeNodes));
-}
-
-void CodeExpander::VisitIf(If * ifNode) {}
-
-void CodeExpander::VisitElseIf(ElseIf * elseIfNode) {}
-
-void CodeExpander::VisitElse(Else * elseNode) {}
-
-void CodeExpander::VisitDeclaration(Declaration * decl) {}
-
-void CodeExpander::VisitRange(Range * range) {}
-
-void CodeExpander::VisitFor(For * forNode) {}
-
-void CodeExpander::VisitFunction(Function * function) {}
-
-void CodeExpander::VisitSignature(Signature * signature) {}
-
-void CodeExpander::VisitFunctionDeclaration(FunctionDeclaration * functionDecl) {}
-
-void CodeExpander::VisitIncludeLib(IncludeLib* includeLib) {}
-
-void CodeExpander::VisitPrintableFromString(PrintableFromString * printStr) {}
-
-void CodeExpander::VisitPrintableFromExpression(PrintableFromExpression * printExpr) {}
-
-void CodeExpander::VisitPrint(Print * print) {}
-
-void CodeExpander::VisitType(Type * type) {}
-
-void CodeExpander::VisitVariable(Variable* variable) {}
-
-void CodeExpander::VisitWhile(While* whileNode) {}
-   
-void CodeExpander::VisitMain(Main* mainNode) {}
