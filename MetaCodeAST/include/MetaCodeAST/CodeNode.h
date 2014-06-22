96b5af6f46b261e425bb75558803e60d977336fd
Ismael-belghiti <isma.belghiti@gmail.com>
2014-06-22 23:05:24 +0200

    minor

--- MetaCodeAST/include/MetaCodeAST/CodeNode.h
+++ MetaCodeAST/include/MetaCodeAST/CodeNode.h
@@ -3,13 +3,22 @@
 
 #include "indentation_space.h"
 
+class AbstractTypeVisitor;
+class AbstractVariableVisitor;
+class AbstractExpressionVisitor;
+class AbstractBlocVisitor;
+class AbstractRangeVisitor;
+class AbstractPrintableVisitor;
+class AbstractStatementVisitor;
+class AbstractFunctionVisitor;
+class AbstractSignatureVisitor;
 class AbstractVisitor;
 
 class CodeNode {
 public:
    virtual void PrintDebug(int level = 0) = 0;
 
-   virtual void Visit(AbstractVisitor* visitor)  = 0;
+   virtual void Visit(AbstractVisitor* visitor) = 0;
 };
 
 #endif
