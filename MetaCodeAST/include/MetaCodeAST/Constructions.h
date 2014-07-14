7642a569faa09b5f829782174dfa249e06c1ebc5
Ismael-belghiti <isma.belghiti@gmail.com>
2014-07-14 11:28:29 +0200

    add Node suffix to constructions file names

--- MetaCodeAST/include/MetaCodeAST/Constructions.h
+++ MetaCodeAST/include/MetaCodeAST/Constructions.h
@@ -1,16 +1,16 @@
 #ifndef CONSTRUCTIONS_H
 #define CONSTRUCTIONS_H
 
-#include "MetaCodeAST/Constructions/Type/Type.h"
-#include "MetaCodeAST/Constructions/Variable/Variable.h"
-#include "MetaCodeAST/Constructions/Expression/Expression.h"
-#include "MetaCodeAST/Constructions/Statement/Compound/While.h"
-#include "MetaCodeAST/Constructions/Statement/Compound/For.h"
-#include "MetaCodeAST/Constructions/Statement/Compound/Conditional.h"
-#include "MetaCodeAST/Constructions/Statement/Compound/Main.h"
-#include "MetaCodeAST/Constructions/Statement/Compound/Function.h"
-#include "MetaCodeAST/Constructions/Statement/Instruction/Declaration.h"
-#include "MetaCodeAST/Constructions/Statement/Instruction/Print.h"
-#include "MetaCodeAST/Constructions/Statement/Instruction/IncludeLib.h"
+#include "MetaCodeAST/Constructions/Type/TypeNode.h"
+#include "MetaCodeAST/Constructions/Variable/VariableNode.h"
+#include "MetaCodeAST/Constructions/Expression/ExpressionNode.h"
+#include "MetaCodeAST/Constructions/Statement/Compound/WhileNode.h"
+#include "MetaCodeAST/Constructions/Statement/Compound/ForNode.h"
+#include "MetaCodeAST/Constructions/Statement/Compound/ConditionalNode.h"
+#include "MetaCodeAST/Constructions/Statement/Compound/MainNode.h"
+#include "MetaCodeAST/Constructions/Statement/Compound/FunctionNode.h"
+#include "MetaCodeAST/Constructions/Statement/Instruction/DeclarationNode.h"
+#include "MetaCodeAST/Constructions/Statement/Instruction/PrintNode.h"
+#include "MetaCodeAST/Constructions/Statement/Instruction/IncludeLibNode.h"
 
 #endif
